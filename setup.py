ffce3a5c14c3a6c958612f1002bcf69c9ece63c3
Alexander Collins <alexcollins010@gmail.com>
2026-05-09 14:26:33 +0300

    v0.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="turbodrf",
-    version="0.4.2",
+    version="0.4.3",
     author="Alexander Collins",
     author_email="",
     description="Dead simple Django REST API generator with role-based permissions",
